fa72a7e9fcda61541ed188e6153cfeb6ed6bc17c
Tyler Black <145217103+blackt117@users.noreply.github.com>
2024-01-16 16:22:48 -0500

    practice

--- CSC231/Just for Fun/Practice.py
+++ CSC231/Just for Fun/Practice.py
@@ -48,12 +48,26 @@ def rec_max(list1):
         else:
             return list1[0]
 
+
+def insert_sort_book(list1):
+    for i in range(1,len(list1)):
+        key = list1[i]
+        j = i - 1
+        while list1[j] > key and j >= 0:
+            list1[j+1] = list1[j]
+            j-=1
+        list1[j+1] = key
+
 list2 = [9,7,-1,0]
 list3=[9,-7,-5,6,3]
+list4 = [0,-2,9,8,10]
 merge_sert(list2)
 insert_sort(list3)
 print(list3)
 print(list2)
 print(rec_max(list3))
+print()
+insert_sort_book(list4)
+print(list4)
 
 
